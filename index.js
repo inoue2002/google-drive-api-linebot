3ea65d948b0924dc90b692b2c2ba84620ffb4c32
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-01-17 16:06:19 +0900

    upload localfile

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 const dotenv = require("dotenv");
 const { google } = require("googleapis");
 const readline = require("readline");
+const axios = require("axios");
 
 dotenv.config();
 
@@ -36,125 +37,114 @@ app.post("/webhook", line.middleware(config), (req, res) => {
 
 const client = new line.Client(config);
 
-const SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"];
+const SCOPES = ["https://www.googleapis.com/auth/drive"];
 const TOKEN_PATH = "token.json";
 
 async function handleEvent(event) {
   if (event.message.type === "image") {
     console.log("画像ID", event.message.id);
     //コンテンツIDからデータを取得
-    await client.getMessageContent(event.message.id).then(async (stream) => {
-      console.log(`1`);
-      console.log(Object.keys(stream));
-      stream.on("data", async (chunk) => {
-        console.log(2);
-        //bufferからbase64にでコード
-        const base64 = chunk.toString("base64");
-        // console.log(base64);
+    //モジュールではなく、axiosを使う
 
-        await fs.readFile("credentials.json", async (err, content) => {
-          // console.log(1);
-          if (err) return console.log("Error loading client secret file:", err);
-          // Authorize a client with credentials, then call the Google Drive API.
-          await authorize(JSON.parse(content), listFiles);
+    const contentsData = await axios.get(
+      `https://api-data.line.me/v2/bot/message/${event.message.id}/content`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`,
+        },
+        data: {},
+      }
+    );
+    //バイナリデータが返される
+    //console.log(contentsData.data);
+
+    //そのままaxiosでアップロードする
+
+    await authorize(listFiles);
+
+    async function authorize(callback) {
+      const oAuth2Client = new google.auth.OAuth2(
+        process.env.client_id,
+        process.env.client_secret,
+        process.env.redirect_uris
+      );
+
+      // Check if we have previously stored a token.
+      fs.readFile(TOKEN_PATH, (err, token) => {
+        if (err) {
+          console.log(err);
+          return getAccessToken(oAuth2Client, callback);
+        }
+        oAuth2Client.setCredentials(JSON.parse(token));
+        callback(oAuth2Client);
+      });
+    }
+    async function getAccessToken(oAuth2Client, callback) {
+      const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: SCOPES,
+      });
+      console.log("Authorize this app by visiting this url:", authUrl);
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      rl.question("Enter the code from that page here: ", (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+          if (err) return console.error("Error retrieving access token", err);
+          oAuth2Client.setCredentials(token);
+          // Store the token to disk for later program executions
+          fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+            if (err) return console.error(err);
+            console.log("Token stored to", TOKEN_PATH);
+          });
+          callback(oAuth2Client);
         });
+      });
+    }
+    function listFiles(auth) {
+      // console.log(2);
+      const drive = google.drive({ version: "v3", auth });
+      //parents: ["1mE19ag5RkacYoggXaEX9Q0YouxHWLo6l"],
 
-        async function authorize(credentials, callback) {
-          const client_secret = process.env.client_secret;
-          const client_id = process.env.client_id;
-          const redirect_uris = process.env.redirect_uris;
-
-          const oAuth2Client = new google.auth.OAuth2(
-            client_id,
-            client_secret,
-            redirect_uris
-          );
-
-          // Check if we have previously stored a token.
-          await fs.readFile(TOKEN_PATH, (err, token) => {
-            if (err) {
-              console.log(err);
-              return getAccessToken(oAuth2Client, callback);
-            }
-            oAuth2Client.setCredentials(JSON.parse(token));
-            callback(oAuth2Client);
-          });
-        }
-
-        async function getAccessToken(oAuth2Client, callback) {
-          const authUrl = oAuth2Client.generateAuthUrl({
-            access_type: "offline",
-            scope: SCOPES,
-          });
-          console.log("Authorize this app by visiting this url:", authUrl);
-          const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-          });
-          rl.question("Enter the code from that page here: ", (code) => {
-            rl.close();
-            oAuth2Client.getToken(code, (err, token) => {
-              if (err)
-                return console.error("Error retrieving access token", err);
-              oAuth2Client.setCredentials(token);
-              // Store the token to disk for later program executions
-              fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log("Token stored to", TOKEN_PATH);
-              });
-              callback(oAuth2Client);
-            });
-          });
-        }
-
-        function listFiles(auth) {
-          // console.log(2);
-          const drive = google.drive({ version: "v3", auth });
-
-          var fileMetadata = {
-            name: "photo.jpg",
-            //parents: ['16ag9BocOs3Fna22jqibXnVcguWvaNiXq']
-          };
-          var media = {
-            mimeType: "image/jpeg",
-            body: fs.createReadStream("files/photo.jpg"),
-          };
-          drive.files.create(
-            {
-              resource: fileMetadata,
-              media: media,
-              fields: "id",
-            },
-            function (err, file) {
-              if (err) {
-                // Handle error
-                console.error(err);
-              } else {
-                console.log("File Id: ", file.id);
-              }
-            }
-          );
-
-          /*
-             drive.files.create(
-              {
-                resource: fileMetadata,
-                media: media,
-                fields: "id",
-              },
-              function (err, file) {
-                if (err) {
-                  // Handle error
-                  console.error(err);
-                } else {
-                  console.log("File Id: ", file.id);
-                }
-              }
-            )
-            */
+      var fileMetadata = {
+        'name': 'photo.jpg'
+      };
+      var media = {
+        mimeType: 'image/jpeg',
+        body: fs.createReadStream('files/photo.jpg')
+      };
+      drive.files.create({
+        resource: fileMetadata,
+        media: media,
+        fields: 'id'
+      }, function (err, file) {
+        if (err) {
+          // Handle error
+          console.error(err);
+        } else {
+          console.log('File Id: ', file.id);
         }
       });
-    });
+      /*
+         drive.files.create(
+          {
+            resource: fileMetadata,
+            media: media,
+            fields: "id",
+          },
+          function (err, file) {
+            if (err) {
+              // Handle error
+              console.error(err);
+            } else {
+              console.log("File Id: ", file.id);
+            }
+          }
+        )
+        */
+    }
   } else {
     //動作確認で鸚鵡返し
     return client.replyMessage(event.replyToken, {
